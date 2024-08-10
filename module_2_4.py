51d359ae62bf38194084f7b35d084bedd316ff10
TrukhanovEgor <161571926+TrukhanovEgor@users.noreply.github.com>
2024-08-10 16:04:58 +0500

    Update module_2_4.py
    исправленный

--- module_2_4.py
+++ module_2_4.py
@@ -2,6 +2,8 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for numbers in numbers:
+    if number == 1:
+        continue
     if numbers < 2:
         not_primes.append(numbers)
         continue
