c1a0c5fe000719c981220d6ee6ce005254a61d2e
TrukhanovEgor <161571926+TrukhanovEgor@users.noreply.github.com>
2024-10-18 16:58:07 +0500

    Add files via upload

---
+++ Module9_2.py
@@ -0,0 +1,21 @@
+def start():
+    first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+    second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+    first_result = [len(item) for item in first_strings if len(item) >= 5]
+    print(first_result)
+
+    second_result = [(first, second)
+                     for first in first_strings
+                     for second in second_strings
+                     if len(first) == len(second)]
+    print(second_result)
+
+    third_result = [(string, len(string))
+                    for string in first_strings + second_strings
+                    if len(string) % 2 == 0]
+    print(third_result)
+
+
+if __name__ == '__main__':
+    start()
